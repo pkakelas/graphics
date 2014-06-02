e933bb96db0be3952d981c665622ff6a1d10f96a
pkakelas <pkakelas@gmail.com>
2014-06-02 13:17:47 +0300

    create a second shm using the shm function

--- shm/shm.js
+++ shm/shm.js
@@ -1,11 +1,13 @@
 var c = document.getElementsByTagName( 'canvas' )[ 0 ];
 var ctx = c.getContext( '2d' );
 var W = 700, H = 700;
-var ball;
+var ball, square;
 var M = 10;
-var RADIUS = 15;
+var RADIUS = 20, SIDE = 40;
 var D = 1;
-var mechEq = 350 - RADIUS
+var Vo = 10; 
+var A = 263;
+var t = -2;
 
 // Hooke law
 function Hooke( x ) {
@@ -19,18 +21,26 @@ function acceleration( f ) {
 
 function init() {
     ball = {
-        x: mechEq,
-        y: 350 - RADIUS,
+        x: W / 2 - RADIUS,
+        y: 320,
         vy: 0,
-        vx: 10 
+        vx: Vo 
+    };
+    square = {
+        x: W / 2 - SIDE,
+        y: 450
     };
 }
 
 function integrate() {
-    f = Hooke( ball.x - mechEq );
+    f = Hooke( ball.x - W / 2 - RADIUS );
     a = acceleration( f );
     ball.vx += a * 15/1000;  
     ball.x += ball.vx;
+
+    w = Math.sqrt( D / M );
+    square.x = A * Math.sin( Math.sqrt( 15 / 1000 ) * w * t   ) + W / 2;
+    ++t;
 }
 
 function render() {
@@ -44,7 +54,11 @@ function render() {
     ctx.arc( ball.x, ball.y, RADIUS, 0, 2 * Math.PI );
     ctx.stroke();
 
-    setTimeout( render, 15 ); //on change line 18 should be changed too
+    ctx.beginPath();
+    ctx.rect( square.x, square.y, SIDE, SIDE );
+    ctx.stroke();
+
+    setTimeout( render, 15 );
 }
 
 init();
