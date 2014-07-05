a3410fc42cce9adf62cb156c28abadc396180aa1
pkakelas <pkakelas@gmail.com>
2014-07-05 12:50:09 +0300

    add gravity oriented fall and 3d cube

--- spring/spring.js
+++ spring/spring.js
@@ -5,7 +5,6 @@ var ball, square;
 var M = 10;
 var RADIUS = 20;
 var D = 1;
-var Vo = 10; 
 
 // Hooke law
 function Hooke( x ) {
@@ -22,8 +21,40 @@ function init() {
         x: W / 2 - RADIUS,
         y: 320,
         vy: 0,
-        vx: Vo 
+        vx: 0 
     };
+}
+
+function renderCeiling() {
+    for ( var i = 0; i < 20; ++i ) {
+        dist = ( W + 20 ) / 20;
+        ctx.beginPath();
+        ctx.moveTo( dist * i, 50 );
+        ctx.lineTo( dist * i + 20, 0 );
+        ctx.stroke();
+    }
+
+    ctx.beginPath();
+    ctx.moveTo( 0, 50 );
+    ctx.lineTo( W, 50 );
+    ctx.stroke();
+}
+
+function renderSpring() {
+    dx = W / 2 - RADIUS;
+    dist = ( ball.y - 50 ) / 10;
+    for ( var i = 0; i < 10; ++i ) {
+        if ( i % 2 == 0 ) {
+            dx = ball.x += 40;
+        }
+        else {
+            dx = ball.x -= 40;
+        }
+        ctx.beginPath();
+        ctx.moveTo( dx, dist );
+        ctx.lineTo( dx, dist );
+        ctx.stroke();
+    }
 }
 
 function integrate() {
@@ -35,17 +66,21 @@ function integrate() {
 
 function render() {
     integrate();
+    renderCeiling();
 
     ctx.fillStyle = '#fff';
-    ctx.fillRect( 0, 0, W, H );
+    ctx.fillRect( 0, 52, W, H );
 
     ctx.fillStyle = 'black';
     ctx.beginPath();
     ctx.arc( ball.x, ball.y, RADIUS, 0, 2 * Math.PI );
     ctx.stroke();
 
+    renderSpring();
+
     setTimeout( render, 15 );
 }
 
+renderCeiling();
 init();
 render();
